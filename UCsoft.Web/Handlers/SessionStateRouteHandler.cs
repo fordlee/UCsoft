66929a4830d73a4e170da4fd79ab5c02a98fc800
terwer <cbgtyw@gmail.com>
2014-11-05 16:59:59 +0800

    配置WebApi支持Session。

---
+++ UCsoft.Web/Handlers/SessionStateRouteHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace UCsoft.Web.Handlers
+{
+    public class SessionStateRouteHandler : IRouteHandler
+    {
+        IHttpHandler IRouteHandler.GetHttpHandler(RequestContext requestContext)
+        {
+            return new SessionableControlHandler(requestContext.RouteData);
+        }
+    }
+}
---
+++ UCsoft.Web/Handlers/SessionableControlHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.WebHost;
+using System.Web.Routing;
+using System.Web.SessionState;
+
+namespace UCsoft.Web.Handlers
+{
+   public class SessionableControlHandler : HttpControllerHandler, IRequiresSessionState
+    {
+        public SessionableControlHandler(RouteData routeData)
+            : base(routeData)
+        {
+        }
+    }
+}
